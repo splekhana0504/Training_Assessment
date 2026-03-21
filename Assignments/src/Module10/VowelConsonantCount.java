d055d0ebf10aa9889a3d8980c49d90bba23e6a76
splekhana0504 <sp.lekhana@gmail.com>
2026-03-21 08:54:25 +0530

    4th assignment

---
+++ Assignments/src/Module10/VowelConsonantCount.java
@@ -0,0 +1,22 @@
+package Module10;
+
+public class VowelConsonantCount {
+    public static void main(String[] args) {
+        String str = "Hello World";
+        int vowels = 0, consonants = 0;
+
+        str = str.toLowerCase();
+
+        for (char c : str.toCharArray()) {
+            if (Character.isLetter(c)) {
+                if ("aeiou".indexOf(c) != -1)
+                    vowels++;
+                else
+                    consonants++;
+            }
+        }
+
+        System.out.println("Vowels: " + vowels);
+        System.out.println("Consonants: " + consonants);
+    }
+}
